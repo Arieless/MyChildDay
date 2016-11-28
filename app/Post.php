faa9b517fdd301b16b05bca8de13894de246b400
bruno-ian <bruno.ian@gmail.com>
2016-11-28 06:32:54 -0300

    sad
    asd

--- app/Post.php
+++ app/Post.php
@@ -10,11 +10,22 @@ class Post extends Model
     protected $fillable = ['contentText'];
 
     public function user() {
-      return $this->belongsTo('App\User', 'user_id');
+      return $this->belongsTo(Class::User, 'user_id');
     }
 
-    public function scopeTop($query) {
+    public function school() {
+      return $this->belongsTo(Class::School, 'school_id');
+    }
+
+    public function postType () {
+      return $this->hasOne(Class::Posttype, 'posttype_id');
+    }
+
+    public function tagged () {
+      return $this->belongsToMany(Class::Kid, 'post_kid', 'post_id', 'kid_id');
+    }
+
+    public function lastTen($query) {
       return $query->take(10)->orderBy('created_at', 'DESC');
     }
-
 }
