3ee6f0f1c0e0f0ba4117c598c0a92ebdfbe0e54c
Ariel Bavasso <arielbavasso@gmail.com>
2016-11-27 22:19:28 -0300

    Edit perfil  responsive (?
    Edit perfil

--- resources/views/layouts/components/footers/home.blade.php
+++ resources/views/layouts/components/footers/home.blade.php
@@ -1,9 +1,7 @@
 <footer class="mainFooter">
   <nav class="mainFooterNav">
     <ul>
-      <li><a href="/faq">FAQ</a></li>
-      <li><a href="/terminos">Politicas de uso</a></li>
-      <li id="popUpContactButton" class="popUpContactButton"><a>Contacto</a></li>
+
     </ul>
     <p>© Untitled. All rights reserved.</p>
   </nav>
